dca7a5a08d58becf3993d6b6a5f12b27913c8c6b
Wonseok Shin <wsshin97@gmail.com>
2013-03-11 18:47:25 -0700

    Commit before roll back to Eprim

--- example/bowtie_tfsf_2d.m
+++ example/bowtie_tfsf_2d.m
@@ -1,4 +1,4 @@
-clear all; close all; clear classes; clc;
+clear all; close all; clear classes; 
 
 %% Set flags.
 isnew = true;
@@ -12,19 +12,20 @@ if isnew
 	dL = 10;
 	dl = 2;
 	wvlen = 200;
+	solveropts.withinterp = false;
 	[E, H, obj_array, src_array, J] = maxwell_run(...
 		'OSC', 1e-9, wvlen, ...
-		'DOM', {'vacuum', 'none', 1.0}, [-610 610; -610 610; 0 dl], [dL dL dl], BC.p, [10*dL 10*dL 0], ...
-		'OBJ', {'vacuum', 'none', 1.0}, Box([-w/2 w/2; -w/2 w/2; 0 dl], dl), ...
+		'DOM', {'vacuum', 'none', 1.0}, [-610 610; -610 610; 0 1], [dL dL 1], BC.p, [10*dL 10*dL 0], ...
+		'OBJ', {'vacuum', 'none', 1.0}, Box([-w/2 w/2; -w/2 w/2; 0 1], [dl dl 1]), ...
 		'SOBJ', ...  % scatter objects
-			{['Johnson', filesep, 'Au'], 'y'}, ...
-				PolygonalCylinder(Axis.z, dl, dl/2, [w/2 0; w/2+s*sqrt(3)/2 -s/2; w/2+s*sqrt(3)/2 s/2], dl), ...
-				PolygonalCylinder(Axis.z, dl, dl/2, [-w/2 0; -w/2-s*sqrt(3)/2 s/2; -w/2-s*sqrt(3)/2 -s/2], dl), ...
-		'SRC', TFSFPlaneSrc([-b b; -b b; 0 dl], Axis.y, Axis.x), ...
-		inspect_only);
+			{['CRC', filesep, 'Ag'], 'y'}, ...
+				PolygonalCylinder(Axis.z, 1, 1/2, [w/2 0; w/2+s*sqrt(3)/2 -s/2; w/2+s*sqrt(3)/2 s/2], [dl dl 1]), ...
+				PolygonalCylinder(Axis.z, 1, 1/2, [-w/2 0; -w/2-s*sqrt(3)/2 s/2; -w/2-s*sqrt(3)/2 -s/2], [dl dl 1]), ...
+		'SRC', TFSFPlaneSrc([-b b; -b b; 0 1], Axis.y, Axis.x), ...
+		solveropts, inspect_only);
 
 	if ~inspect_only
-		save(mfilename, 'E', 'H', 'obj_array');
+% 		save(mfilename, 'E', 'H', 'obj_array');
 	end
 else
 	load(mfilename);
@@ -39,9 +40,14 @@ opts.withabs = true;
 % opts.withgrid = true;
 % opts.cscale = 1e-1;
 z_location = 0;
-vis2d(E{Axis.x}, Axis.z, z_location, obj_array, src_array, opts)
+% vis2d(E{Axis.x}, Axis.z, z_location, obj_array, src_array, opts)
 % vis2d(H{Axis.z}, Axis.z, z_location, obj_array, src_array, opts)
 
 % %% Calculate the power emanating from the source.
-% power = powerflux_box(E,H,[-10 10; -10 10; 0 1]);
+% Exmax = max(abs(E{Axis.x}.array(:)));
+% Hzmax = max(abs(H{Axis.z}.array(:)));
+Exmax = max(abs(E{Axis.x}(:)));
+Hzmax = max(abs(H{Axis.z}(:)));
+fprintf('Exmax = %e, Hzmax = %e\n', Exmax, Hzmax);
+% power = powerflux_box(E,H,[-b-10 b+10; -b-10 b+10; 0 1]);
 % fprintf('power = %e\n', power);
