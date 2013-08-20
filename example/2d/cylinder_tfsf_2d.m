e75827bf8e7a65e21f6344cafdf6ded6886df681
Wonseok Shin <wsshin97@gmail.com>
2013-08-19 19:55:14 -0700

    the scattering cross section is correctly divided by thickness of the 3D simulation in example/2d/cylinder_tfsf_2d.m.

--- example/2d/cylinder_tfsf_2d.m
+++ example/2d/cylinder_tfsf_2d.m
@@ -11,17 +11,20 @@ dl = 2;  % fine grid cell size
 r = 100;  % radius
 s = 200;  % semiside of TF/SF box
 b = s + 200 + 10*dL;  % semiside of simulation domain (10 grid cells inside PML)
+sm = s + 100;
 
 wvlen = 1550;  % wavelength
 
 [E, H, obj_array, src_array, J] = maxwell_run(...
 	'OSC', L0, wvlen, ...
 	'DOM', {'vacuum', 'none', 1.0}, [-b b; -b b; 0 dL], dL, BC.p, [10*dL 10*dL 0], ...
+	'OBJ', {'vacuum', 'blue', 1.0}, Box([-sm sm; -sm sm; 0 dL]), ...
 	'SOBJ', ...  % scattering objects
 		{'Johnson/Au', 'y'}, ...
 			CircularCylinder(Axis.z, dL, [0 0 dL/2], r, [dl dl dL]), ...
 	'SRCJ', TFSFPlaneSrc([-s s; -s s; 0 dL], Axis.y, Axis.x), ...  % normal direction is y; polarization direction is x
 	inspect_only);
+
 
 if ~inspect_only
 	%% Visualize the solution.
@@ -38,7 +41,6 @@ if ~inspect_only
 	% vis2d(H{Axis.z}, Axis.z, z_location, obj_array, src_array, opts)
 
 	%% Calculate the scatterd power
-	sm = s + 10*dL;  % semiside of measurement box
 	s_power = powerflux_box(E,H, [-sm sm; -sm sm; 0 dL]);
-	fprintf('scattering cross section = %e\n', s_power);  % intensity of incident wave = 1
+	fprintf('scattering cross section = %e\n', s_power/dL);  % intensity of incident wave = 1
 end
