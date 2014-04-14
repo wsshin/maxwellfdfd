9fe6809f2c5d7225f1537580b0dedd47efac2b65
Wonseok Shin <wsshin97@gmail.com>
2014-04-14 16:53:22 -0700

    deriv/create_Dw.m is simplified using circshift

--- deriv/create_Dw.m
+++ deriv/create_Dw.m
@@ -9,29 +9,15 @@ chkarg(istypesizeof(w, 'Axis') || istypesizeof(w, 'Dir'), '"w" should be instanc
 chkarg(istypesizeof(N, 'int', [1 w.count]), '"N" should be length-%d row vector with integer elements.', w.count);
 chkarg(istypesizeof(fg, 'complex'), '"nL" should be complex.');
 
-
-shift = zeros(1, w.count);
-shift(w) = 1;
-
-% Get the displaced spatial markers.
-ind_next = @(n, s) mod((1:n) + s - 1, n) + 1;
-if istypeof(w, 'Axis')
-	[i_next, j_next, k_next] = ndgrid(...
-		ind_next(N(Axis.x), shift(Axis.x)), ...
-		ind_next(N(Axis.y), shift(Axis.y)), ...
-		ind_next(N(Axis.z), shift(Axis.z)));
-else
-	[i_next, j_next] = ndgrid(...
-		ind_next(N(Dir.h), shift(Dir.h)), ...
-		ind_next(N(Dir.v), shift(Dir.v)));
-	k_next = 1;
-end
-
-% Translate spatial indices into matrix indices.
+% Translate spatial indices (i,j,k) into matrix indices.
 M = prod(N);
 row_ind = 1:M;
 col_ind_curr = 1:M;
-col_ind_next = i_next + (j_next-1) * N(Axis.x) + (k_next-1) * N(Axis.x) * N(Axis.y);
+
+col_ind_next = reshape(1:M, N);
+shift = zeros(1, w.count);
+shift(w) = -1;
+col_ind_next = circshift(col_ind_next, shift);
 
 a_curr = ones(N);
 a_ind = {':', ':', ':'};
